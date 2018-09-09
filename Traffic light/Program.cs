52f97271ef6a048c641f771a35ca15e8ecce36c8
marmeladhero <marmeladhero@gmail.com>
2018-09-09 21:18:39 +0300

    Добавьте файлы проекта.

---
+++ Traffic light/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Traffic_light
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static Queue<Human> humans;
+
+        static void Main(string[] args)
+        {
+            TrafficLight trafficLight = new TrafficLight();
+            trafficLight.LightChanged += TrafficLight_lightChanged;
+            humans = new Queue<Human>(GenerateHumans(10));
+
+            while (humans.Count > 0)
+            {
+                trafficLight.ChangeState();
+            }
+
+            Console.ReadKey();
+                        
+        }
+
+        /// <summary>
+        /// True - red, False - Green
+        /// </summary>
+        /// <param name="state"></param>
+        private static void TrafficLight_lightChanged(bool state)
+        {
+            if (state != true)
+                Console.WriteLine($"{humans.Dequeue().Name} passed");
+        }
+
+        static IEnumerable<Human> GenerateHumans(int count)
+        {
+            List<Human> lstHumans = new List<Human>();
+            for(int i = 0; i < count; i++)
+            {
+                lstHumans.Add(new Human($"People #{i + 1}"));
+            }
+            return lstHumans;
+        }
+    }
+}
---
+++ Traffic light/Traffic light.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Traffic_light
+{   
+    class TrafficLight
+    {
+        public delegate void DelStateChange(bool state);
+        public event DelStateChange LightChanged;
+
+        private static Random randChangeLight = new Random();
+
+        public string Color { get; private set; }
+        private bool _IsRed = true;
+
+        public void ChangeState()
+        {
+            _IsRed = randChangeLight.Next(100) <= 20;
+
+            if (_IsRed)
+            {
+                _IsRed = false;
+                Color = "Green";
+            }
+            else
+            {
+                _IsRed = true;
+                Color = "Red";
+            }
+
+            Console.WriteLine(Color);
+            LightChanged(_IsRed);
+        }
+    }
+
+
+    class Human
+    {
+        public string Name { get; private set; }        
+        public Human(string strName) => Name = strName;                
+    }
+}
